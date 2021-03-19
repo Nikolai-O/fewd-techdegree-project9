6a1f4b3067765315f432ec15db5a8c8743889bda
Nikolai-O <70658689+Nikolai-O@users.noreply.github.com>
2021-03-19 09:57:04 -0400

    add basic structure
    This is the first commit that includes the basic struture and features of the website. The about section still needs to be implemented.

---
+++ script/app.js
@@ -0,0 +1,24 @@
+const about = document.getElementById('about');
+const aboutBtn = document.getElementById('aboutBtn');
+const title1 = document.getElementById('title1');
+const title2 = document.getElementById('title2');
+const about1 = document.getElementById('about1');
+const about2 = document.getElementById('about2');
+
+
+window.onscroll = ( e => {
+    if (window.scrollY > 1900) {
+        aboutBtn.style.display = "block";
+    }
+});
+
+function displayModal() {
+    const modalHTML = `
+        
+    `
+}
+
+// aboutBtn.addEventListener('click', e => {
+
+// });
+
