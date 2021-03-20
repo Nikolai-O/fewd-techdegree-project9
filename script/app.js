d0a79f1c07dea8b0ceda1a6e69b0cc876709fa20
Nikolai-O <70658689+Nikolai-O@users.noreply.github.com>
2021-03-19 20:37:34 -0400

    implement JS
    The about section now includes JavaScript to display modals.

--- script/app.js
+++ script/app.js
@@ -1,24 +1,54 @@
 const about = document.getElementById('about');
 const aboutBtn = document.getElementById('aboutBtn');
-const title1 = document.getElementById('title1');
-const title2 = document.getElementById('title2');
-const about1 = document.getElementById('about1');
-const about2 = document.getElementById('about2');
+const overlay1 = document.getElementById('overlay1');
+const modalContent = document.getElementById('modalContent')
+const modalClose1 = document.getElementById("modalClose1");
+const modalClose2 = document.getElementById("modalClose2");
+const next = document.getElementById("modalNext");
+const previous = document.getElementById("modalPrevious");
 
 
 window.onscroll = ( e => {
-    if (window.scrollY > 1900) {
+    if (window.scrollY > 1900 & window.matchMedia("(min-width: 768px)").matches) {
         aboutBtn.style.display = "block";
     }
 });
 
-function displayModal() {
-    const modalHTML = `
-        
-    `
+window.addEventListener('resize', e => {
+    if (window.matchMedia("(max-width: 767px)").matches) {
+        aboutBtn.style.display = "none";
+        overlay1.style.display = "none";
+        overlay2.style.display = "none";
+    }
+})
+
+function displayModal1() {
+    overlay1.style.display = "block";
+    overlay2.style.display = "none";
 }
 
-// aboutBtn.addEventListener('click', e => {
+function displayModal2() {
+    overlay1.style.display = "none";
+    overlay2.style.display = "block";
+}
 
-// });
+aboutBtn.addEventListener('click', e => {
+    aboutBtn.style.display = "none";
+    displayModal1();
+});
 
+next.addEventListener('click', e => {
+    displayModal2();
+});
+
+previous.addEventListener('click', e => {
+    displayModal1();
+});
+
+modalClose1.addEventListener('click', e => {
+    overlay1.style.display = "none";
+});
+
+modalClose2.addEventListener('click', e => {
+    overlay2.style.display = "none";
+});
